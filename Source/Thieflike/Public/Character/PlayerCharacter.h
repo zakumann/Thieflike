45f79d84cb42c7ffd770134a02f75d0e1b27eabb
zakumann <samuel742able@gmail.com>
2025-12-10 00:02:32 +0900

    Add Look function and CameraComponent

--- Source/Thieflike/Public/Character/PlayerCharacter.h
+++ Source/Thieflike/Public/Character/PlayerCharacter.h
@@ -3,8 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
 #include "GameFramework/Character.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h" 
+#include "InputActionValue.h"
 #include "PlayerCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+class UInputComponent;
 
 UCLASS()
 class THIEFLIKE_API APlayerCharacter : public ACharacter
@@ -19,6 +27,21 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* FirstPersonContext;
+
+	// Move Input Actions
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* MoveAction;
+
+	// Look Input Actions
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* LookAction;
+
+	// Jump Input Actions
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* JumpAction;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -26,4 +49,31 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	// Handles 2D Movement Input
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
+
+	// Handles Look Input
+	UFUNCTION()
+	void Look(const FInputActionValue& Value);
+
+	// First Person camera
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	UCameraComponent* FirstPersonCameraComponent;
+
+	// Offset for the first-person camera
+	UPROPERTY(EditAnywhere, Category = Camera)
+	FVector FirstPersonCameraOffset = FVector(0.0f, 0.0f, 40.0f);
+
+	// First-person primitives field of view
+	UPROPERTY(EditAnywhere, Category = Camera)
+	float FirstPersonFieldOfView = 70.0f;
+
+	// First-person primitives view scale
+	UPROPERTY(EditAnywhere, Category = Camera)
+	float FirstPersonViewScale = 0.6f;
+
+	// First-person mesh, visible only to the owning player
+	UPROPERTY(VisibleAnywhere, Category = Mesh)
+	USkeletalMeshComponent* FirstPersonMeshComponent;
 };
